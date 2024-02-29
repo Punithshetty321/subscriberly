036d99ecf21f16eb74118eb875ff6a5da0334c1f
punithshetty321 <puni0692@gmail.com>
2024-02-29 09:20:17 +0530

    created navbar component

--- client/pages/index.jsx
+++ client/pages/index.jsx
@@ -1,48 +1,11 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import styles from '../styles/styles.module.css';
-import { Router, useRouter } from 'next/router';
-import { useAuth } from './AuthContext';
+import Navbar from './Navbar';
 
 const Home = () => {
-  const router = useRouter();
-  const{isLoggedIn, logout, navigateToAddProduct, navigateToLogin, navigateToRegister } = useAuth();
-
-   
-    const handleLogout = () => {
-      logout();
-      router.push('/auth/login');
-    };
   return (
     <div>
-        
-      <header className={styles.header}>
-        <nav className={styles.nav}>
-          <div className={styles.logo}>Your Logo</div>
-          <div className={styles.navLinks}>
-            <a href="/">Home</a>
-            <a href="/categories">Categories</a>
-            <div className={styles.dropdown}>
-              <label htmlFor="sort-by">Sort By:</label>
-              <select id="sort-by">
-                <option value="latest">Latest</option>
-                <option value="popular">Popular</option>
-              </select>
-            </div>
-          </div>
-          <div className={styles.authButtons}>
-            {isLoggedIn ?(
-              <><button onClick={navigateToAddProduct}>AddProduct</button><button onClick={handleLogout}>Logout</button></>
-              
-            ):(
-              <>
-            
-            <button onClick={navigateToRegister}>Register</button>
-            <button onClick={navigateToLogin}>Login</button>
-            </>
-            )}
-          </div>
-        </nav>
-      </header>
+      <Navbar />
 
       <main className={styles.main}>
         {/* Your page content goes here */}
